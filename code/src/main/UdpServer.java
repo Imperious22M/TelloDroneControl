cd71d2f9a782fcbd8cbce3f56f36650a454e0538
John Jay HS <scottroelker20@gmail.com>
2021-07-16 21:12:19 -0500

    independance Complete
    UdpServer class now works by itself. UDP functionality is functional without Tello library.

--- code/src/main/UdpServer.java
+++ code/src/main/UdpServer.java
@@ -1,58 +1,138 @@
 package src.main;
 
+import com.tello.logger.Logger;
+import java.io.IOException;
 import java.net.*;
-
-import java .io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class UdpServer {
-    private int port;
-    private DatagramSocket udpSocket;
+    private final String host; // The Tello
+    private final int hostPort;
+    private final int clientPort;
+    private InetAddress address;
+    private DatagramSocket socket;
 
-    public UdpServer() throws SocketException,IOException{
-        this.port = 8890;
-        udpSocket = new DatagramSocket(this.port);
-    }
-    public UdpServer(int port) throws SocketException,IOException{
-        this.port = port;
-        udpSocket  = new DatagramSocket(this.port);
-    }
+    public UdpServer(){ //Default socket set for listening to Tello telemetry
+        this.host = "192.168.10.1";
+        this.clientPort = 8890;
+        this.hostPort = 0000;
 
-    public String listen() throws Exception{
-        byte[] buf = new byte[1024];
-        final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        String retMsg  = "";
-
-        udpSocket.receive(packet);
-        
-        retMsg = new String(packet.getData());
-
-        return retMsg;
+        try{
+            this.address = InetAddress.getByName(host);
+            this.socket = new DatagramSocket(clientPort);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String sendAndReceive(InetAddress address, String msg) throws Exception{
-        //byte[] buf = new byte[1024];
-        final DatagramPacket sendPacket = new DatagramPacket(msg.getBytes(),msg.getBytes().length , address, 8889);
-        //final DatagramPacket receivPacket = new DatagramPacket(buf, buf.length);
-        String ret = "";
 
-        udpSocket.send(sendPacket);
+    public UdpServer(String host, int hostPort, int clientPort) {
+        this.host = host;
+        this.hostPort = hostPort;
+        this.clientPort = clientPort;
 
-        ret = this.listen();
-        return ret;
+        try {
+            this.address = InetAddress.getByName(host);
+            this.socket = new DatagramSocket(clientPort);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        this.connect();
+    }
+
+    /**
+     * Close connection
+     * THIS METHODS NEEDS TO BE CALLED WHEN EXITING
+     */
+    public void close() {
+        socket.close();
+    }
+
+    private void connect() {
+        this.socket.connect(this.address, this.hostPort);
+    }
+
+    public String sendAndReceiveCommand(String command, int length) {
+        this.sendCommand(command);
+        return this.receiveMessage(length);
+    }
+
+    public String sendAndReceiveCommand(String command) {
+        this.sendCommand(command);
+        return this.receiveMessage();
+    }
+
+    public boolean confirmationCommand(String command) {
+        String response = sendAndReceiveCommand(command);
+        System.out.println(response.length());
+        if(response.equalsIgnoreCase("ok")) {
+            return true;
+        }
+        return false;
+    }
+
+    public void sendCommand(String command) {
+        if(this.socket.isConnected()) {
+            if(command != null) {
+                if(command.length() > 0) {
+                    final byte[] data = command.getBytes();
+                    final DatagramPacket packet = new DatagramPacket(data, data.length, address, hostPort);
+                    try {
+                        Logger.INSTANCE.information("Sending tello command: " + command);
+                        socket.send(packet);
+                    } catch (IOException e) {
+                        Logger.INSTANCE.error("Error when sending packet" + host + ":" + hostPort, e);
+                    }
+                }else {
+                    Logger.INSTANCE.error("Command is empty!");
+                }
+            }else {
+                Logger.INSTANCE.error("Command is null!");
+            }
+        }else {
+            Logger.INSTANCE.error("UDP Socket is not connected! " + host + ":" + hostPort);
+        }
+    }
+
+    public String receiveMessage() {
+        return this.receiveMessage(1024);
+    }
+
+    public String receiveMessage(int length) {
+        byte[] receiveData = new byte[1024];
+        final DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        try {
+            Logger.INSTANCE.information("Waiting for response... Length: " + length);
+            socket.receive(receivePacket);
+            return trimExecutionResponse(receiveData, receivePacket);
+        } catch (IOException e) {
+            Logger.INSTANCE.error("Error when receiving packet" + host + ":" + hostPort, e);
+        }
+        return null;
+    }
+
+    private String trimExecutionResponse(byte[] response, DatagramPacket receivePacket) {
+        response = Arrays.copyOf(response, receivePacket.getLength());
+        return new String(response, StandardCharsets.UTF_8);
     }
 
     public static void main(String[] args) throws Exception{
-        UdpServer ser = new UdpServer(62704); // port working was 62704
+        UdpServer ser = new UdpServer("192.168.10.1",8889,62704); // port working was 62704
         // test send and recieve of tello battery message (send and receive, make sure ip and port is correct)        
         System.out.println(InetAddress.getAllByName("10.0.0.1")[0].toString());
        
-        System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "command"));
-        System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "speed 100"));
-        System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "battery?"));
-        System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "sdk?"));
-       System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "time?"));
-       System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "wifi?"));
-       System.out.println( ser.sendAndReceive(InetAddress.getAllByName("192.168.10.1")[0], "speed?"));
+        System.out.println( ser.sendAndReceiveCommand("command"));
+        System.out.println( ser.sendAndReceiveCommand("speed 100"));
+        System.out.println( ser.sendAndReceiveCommand("battery?"));
+        System.out.println( ser.sendAndReceiveCommand("sdk?"));
+       System.out.println( ser.sendAndReceiveCommand("time?"));
+       System.out.println( ser.sendAndReceiveCommand("wifi?"));
+       System.out.println( ser.sendAndReceiveCommand("speed?"));
        
        /*
         while(true){
@@ -60,6 +140,7 @@ public class UdpServer {
         }
         */
     }
+
 
 
 }
